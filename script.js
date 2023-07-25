7a1890ff812e0136f32128bdc0c506bb810c25ea
Shreyansh Pandey <110768715+shreyansh-pande@users.noreply.github.com>
2023-07-25 17:24:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const smMenuBtn = document.querySelector('.navbtn')
+const smMenu = document.querySelector('.SmallMenu')
+const smMenuCloseBtn = document.querySelector('.SmallMenu-close')
+
+const smMenuLinks = document.querySelectorAll('.SmallMenu-link')
+const smMenuLink1 = document.querySelector('.SmallMenu-link--1')
+const smMenuLink2 = document.querySelector('.SmallMenu-link--2')
+const smMenuLink3 = document.querySelector('.SmallMenu-link--3')
+const smMenuLink4 = document.querySelector('.SmallMenu-link--4')
+
+smMenuBtn.addEventListener('click', () => {
+  smMenu.style.transitionDelay = '0s'
+  smMenu.classList.add('SmallMenu--active')
+
+  smMenuLink1.style.transitionDelay = '.5s'
+  smMenuLink1.style.transform = 'translateY(0)'
+  smMenuLink1.style.opacity = '1'
+
+  smMenuLink2.style.transitionDelay = '.8s'
+  smMenuLink2.style.transform = 'translateY(0)'
+  smMenuLink2.style.opacity = '1'
+
+  smMenuLink3.style.transitionDelay = '1.1s'
+  smMenuLink3.style.transform = 'translateY(0)'
+  smMenuLink3.style.opacity = '1'
+
+  smMenuLink4.style.transitionDelay = '1.4s'
+  smMenuLink4.style.transform = 'translateY(0)'
+  smMenuLink4.style.opacity = '1'
+})
+
+smMenuLinks.forEach((ele) => {
+  ele.addEventListener('click', () => {
+    smMenuLink4.style.transitionDelay = '0s'
+    smMenuLink4.style.transform = 'translateY(50px)'
+    smMenuLink4.style.opacity = '0'
+
+    smMenuLink3.style.transitionDelay = '.3s'
+    smMenuLink3.style.transform = 'translateY(50px)'
+    smMenuLink3.style.opacity = '0'
+
+    smMenuLink2.style.transitionDelay = '.6s'
+    smMenuLink2.style.transform = 'translateY(50px)'
+    smMenuLink2.style.opacity = '0'
+
+    smMenuLink1.style.transitionDelay = '.9s'
+    smMenuLink1.style.transform = 'translateY(50px)'
+    smMenuLink1.style.opacity = '0'
+
+    smMenu.style.transitionDelay = '1.2s'
+    smMenu.classList.remove('SmallMenu--active')
+
+    setTimeout(() => {
+      document.getElementById(ele.name).scrollIntoView()
+    }, 1300)
+  })
+})
+
+smMenuCloseBtn.addEventListener('click', () => {
+  smMenuLink4.style.transitionDelay = '0s'
+  smMenuLink4.style.transform = 'translateY(50px)'
+  smMenuLink4.style.opacity = '0'
+
+  smMenuLink3.style.transitionDelay = '.3s'
+  smMenuLink3.style.transform = 'translateY(50px)'
+  smMenuLink3.style.opacity = '0'
+
+  smMenuLink2.style.transitionDelay = '.6s'
+  smMenuLink2.style.transform = 'translateY(50px)'
+  smMenuLink2.style.opacity = '0'
+
+  smMenuLink1.style.transitionDelay = '.9s'
+  smMenuLink1.style.transform = 'translateY(50px)'
+  smMenuLink1.style.opacity = '0'
+
+  smMenu.style.transitionDelay = '1.2s'
+  smMenu.classList.remove('SmallMenu--active')
+})
